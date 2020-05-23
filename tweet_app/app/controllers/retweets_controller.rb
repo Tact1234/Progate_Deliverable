c50c18c8ed1092cf823dab8ebbbfbc698d2e0354
Tact1234 <andliwake@yahoo.co.jp>
2020-05-23 22:17:55 +0900

    retweet2

---
+++ tweet_app/app/controllers/retweets_controller.rb
@@ -0,0 +1,21 @@
+class RetweetsController < ApplicationController
+  def create
+    @retweet = Retweet.new(
+      user_id: @current_user.id,
+      post_id: params[:post_id]
+    )
+    @retweet.save
+    #redirect_to("/posts/#{params[:post_id]}")
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @retweet = Retweet.find_by(
+      user_id: @current_user.id,
+      post_id: params[:post_id]
+    )
+    @retweet.destroy
+    #redirect_to("/posts/#{params[:post_id]}")
+    redirect_to("/posts/index")
+  end
+end
